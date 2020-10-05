549402aac546b977b1bc514686c35b48c21dff77
Moses <loschnigsolutions@gmail.com>
2020-10-05 21:54:44 +0530

    Added a Script to download required packages

---
+++ install/init.sh
@@ -0,0 +1,20 @@
+
+##############################################
+#                                            #
+#	Run ./init.sh to run the script      #
+#                                            #
+##############################################
+
+
+### ST-Tools ###
+
+sudo apt-get install stlink-tools
+
+### On Chip Debugger ###
+
+sudo apt-get install openocd
+
+### Cross Toolchain ###
+
+sudo apt-get install gcc-arm-none-eabi
+
